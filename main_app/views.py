1186756c8601112a4eaabc16f2a3c3678e76fffb
lovedrones <JuanTron@Juans-MacBook-Pro.local>
2020-01-02 16:30:36 -0600

    layout fix. added to details page

--- main_app/views.py
+++ main_app/views.py
@@ -4,15 +4,20 @@ from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .models import Patient
+
 # Create your views here.
+
+
 
 def home(request):
     return render(request, 'home.html')
 def login(request):
     return render(request, 'login.html')
-def patients_details(request):
+def patients_detail(request, patient_name):
     return render(request, 'patients/details.html')
 def patients_index(request):
-    return render(request, 'patients/index.html')
+    patients = Patient.objects.filter(user = request.user) 
+    return render(request, 'patients/index.html', { 'patients' : patients } )
 def registration_signup(request):
     return render(request, 'registration/signup.html')
